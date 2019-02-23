6f2492b5244409f06fd0a008ea4016ca11775f2f
EngineerAhmedMo <engineer.ahmedhussein@gmail.com>
2019-02-23 03:30:15 +0200

    Edit Basic Fields in Profile (Sec1) using MVC.AJAX

--- ListedIN/Controllers/ProfileController.cs
+++ ListedIN/Controllers/ProfileController.cs
@@ -23,15 +23,43 @@ namespace ListedIN.Controllers
 
         public ActionResult Index()
         {
-           var loggedId = HttpContext.User.Identity.GetUserId();
-
-
-           var user = _context.Users.Single(c => c.Id == loggedId);
+            var loggedId = HttpContext.User.Identity.GetUserId();
+            var user = _context.Users.Single(c => c.Id == loggedId);
 
             return View(user);
         }
 
-     
+
+        [HttpPost]
+        public ActionResult EditSec1(ApplicationUser user)
+        {
+            var userEdit = _context.Users.Find(user.Id);
+
+            if (user.FirstName != null || user.LastName != null)
+            // AutoMapper is helpful here !
+            {
+                userEdit.FirstName = user.FirstName;
+                userEdit.LastName = user.LastName;
+                userEdit.Position = user.Position;
+                userEdit.HeadLine = user.HeadLine;
+                userEdit.Summary = user.Summary;
+                userEdit.Country = user.Country;
+                _context.SaveChanges();
+                return PartialView("_Partial_Sec1", userEdit);
+
+            }
+
+            return View("Index", userEdit);
+
+
+        }
+
+
+      
+
+
+
+
 
     }
 }
