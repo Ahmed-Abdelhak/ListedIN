0474ff2ca2b8d07c55f6b3015d330da11f11e0fd
EngineerAhmedMo <engineer.ahmedhussein@gmail.com>
2019-02-25 10:05:37 +0200

    Implement Search function

---
+++ ListedIN/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using ListedIN.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ListedIN.Controllers
+{
+    [AllowAnonymous]
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SearchController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _context.Dispose();
+        }
+
+        public ActionResult Index(string first,string last)
+        {
+            if (first != null || last != null)
+            {
+                var users = _context.Users.Where(u => u.FirstName.StartsWith(first) || u.LastName.StartsWith(last))
+                    .ToList();
+                return View(users);
+            }
+            return View();
+
+        }
+    }
+}
